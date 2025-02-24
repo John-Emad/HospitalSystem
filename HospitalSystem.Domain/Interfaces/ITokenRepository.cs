0e2e35a31d9ea9807fcf25b5c1e95fc14a5ae8ef
John-Emad <john_emad7@outlook.com>
2025-02-24 18:37:08 +0200

    removing unused libraries and namespaces

--- HospitalSystem.Domain/Interfaces/ITokenRepository.cs
+++ HospitalSystem.Domain/Interfaces/ITokenRepository.cs
@@ -1,10 +1,5 @@
 ﻿using HospitalSystem.Domain.Entities;
 using HospitalSystem.Domain.Entities.People;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HospitalSystem.Domain.Interfaces
 {
